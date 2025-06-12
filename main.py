073e24550d747425a10cd7e94c3968b587a2116a
JLaufer1994 <Jss.Laufer@gmail.com>
2025-06-12 17:11:54 -0400

    Word number

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from stats import get_book_text
 
+def get_book_word_number(path):
+    book_text = get_book_text(path)
+    return len(book_text.split())
+
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
-    print(book_text)
+    num_words = get_book_word_number('books/frankenstein.txt')
+    print(str(num_words) + " words found in the document")
 
 main()
