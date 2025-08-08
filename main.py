f0c19e62120313ae280a2562497b66fefcb47ef4
Jesse Laufer <jss.laufer@gmail.com>
2025-08-08 20:57:37 +0000

    Remove harcoded book path and replace with cli arguments

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from stats import (char_frequency, count_characters, count_words,
                    get_book_text, sorted_char_frequency)
+import sys
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    # Get the path to the book text from command line arguments.
+    book_name = sys.argv[1]
+
     # Read the book text from file.
-    text = get_book_text('books/frankenstein.txt')
+    text = get_book_text(book_name)
     
     # Count the number of words.
     num_words = count_words(text)
@@ -20,7 +28,7 @@ def main():
     
     # Output all results.
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_name}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words.")
     print("-------- Character Count -------")
